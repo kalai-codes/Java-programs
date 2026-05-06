dc6feec12e2340f2317ddb7d1a4aeffa45ff328f
Kalainilavarasu D <kalainilavarasudcse2024@jerusalemengg.ac.in>
2026-05-06 22:09:38 +0530

    Add files via upload

---
+++ stringexample.java
@@ -0,0 +1,10 @@
+package javafundamentals;
+
+import java.util.Scanner;
+public class stringexample {
+   public static void main(String[] args) {
+	   Scanner sc=new Scanner(System.in);
+	   String s=sc.nextLine();
+	   System.out.println("Welcome "+s);
+   }
+}
