0e32fbd347bd4cffd3901f8461518028341f5bab
Kalainilavarasu D <kalainilavarasudcse2024@jerusalemengg.ac.in>
2026-06-09 15:06:40 +0530

    Update numcrunch7.java

--- numcrunch7.java
+++ numcrunch7.java
@@ -1,7 +1,7 @@
 package javafundamentals;
 
 import java.util.Scanner;
-public class palindromeno {
+public class numcrunch7 {
    public static void main(String[] args) {
 	   Scanner sc=new Scanner(System.in);
 	   int n=sc.nextInt();
