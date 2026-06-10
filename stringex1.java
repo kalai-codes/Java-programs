80a3420547040a606de5f874a3677b116a17e84b
Kalainilavarasu D <kalainilavarasudcse2024@jerusalemengg.ac.in>
2026-06-10 12:42:01 +0530

    Add files via upload

---
+++ stringex1.java
@@ -0,0 +1,12 @@
+
+//lower case alphabets
+
+package javafundamentals;
+
+public class stringex1 {
+public static void main(String[] args) {
+	for(char ch = 'a'; ch <= 'z'; ch++) {
+        System.out.print(ch + " ");
+    }
+}
+}
---
+++ stringex2.java
@@ -0,0 +1,12 @@
+
+//upper case alphabets
+
+package javafundamentals;
+
+public class stringex2 {
+public static void main(String[] args) {
+	for(char ch = 'A'; ch <= 'Z'; ch++) {
+        System.out.print(ch + " ");
+    }
+}
+}
